6a781befda1f4491f3e2b33997d7bf0287f80798
Leon <hualiang.tian@gmail.com>
2022-01-27 21:24:08 -0500

    commit tree 7d00507fed8234859130f69e47f403810e101e4a

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,9 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    // return movieData.map((movie, index) => <MovieCard key={index} movieInfo={movie} />)
+    return movieData.map((movie) => <MovieCard poster={movie.poster} title={movie.title} genres={movie.genres} IMDBRating={movie.IMDBRating} />)
+
   }
 
   render() {
